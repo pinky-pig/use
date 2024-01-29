05bfc1149989f40464dcac8e04768142b6694738
pinky-pig <2545589993@qq.com>
2024-01-29 16:06:48 +0800

    feat: customize my-cover

--- cover/src/components/StickerOne.vue
+++ cover/src/components/StickerOne.vue
@@ -7,12 +7,12 @@
     <div class="box pointer-events-none h-full w-full flex flex-col items-center justify-center rounded-md bg-[var(--cover-main-color)] text-center font-bold ">
       <!-- 椭圆 -->
       <div class="grid w-full flex-1 flex-shrink-0 place-items-center px-[30px]">
-        <div contenteditable class="h-[calc(100%_-_60px)] w-full rounded-[50px] bg-white leading-[60px] flex justify-center items-center text-[36px]">
+        <div contenteditable class="h-3/4 w-full rounded-[50px] bg-white leading-[60px] flex justify-center items-center text-[36px]">
           {{ `Arvin` }}
         </div>
       </div>
       <!-- 描述 -->
-      <div contenteditable class="h-1/4 max-h-[42px] w-full flex-shrink-0 border-4 border-[var(--cover-main-color)] rounded-md border-solid bg-white p-[2px] flex justify-center items-center">
+      <div contenteditable class="h-1/4 w-full flex-shrink-0 border-4 border-[var(--cover-main-color)] rounded-md border-solid bg-white p-[2px] flex justify-center items-center">
         mmeme.me
       </div>
     </div>
