2f5496cb1453870769563dc5a6d652857f4dd0fc
pinky-pig <2545589993@qq.com>
2024-02-21 14:54:50 +0800

    feat: 添加“鼓励一下”

--- cover/src/App.vue
+++ cover/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Cover from './components/Cover.vue'
+// import MoneyCard from './components/money-card/index.vue'
 </script>
 
 <template>
   <div class="w-screen h-screen">
+    <!-- <MoneyCard></MoneyCard> -->
     <Cover></Cover>
   </div>
 </template>
